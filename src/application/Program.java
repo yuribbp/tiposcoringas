96b7880f88267e4531512d290e916062842d2b74
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 12:02:07 -0300

    Implementation Any Type with String

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,9 @@ public class Program {
 	public static void main(String[] args) {
 		List<Integer> myInts = Arrays.asList(5, 2, 10);
 		printList(myInts);
+		
+		List<String> myStrs = Arrays.asList("Maria","Alex","Bob");
+		printList(myStrs);
 	}
 
 	public static void printList(List<?> list) {

4fdd350fedf45e92a4f54397243f2b1cb56d18a6
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 11:52:52 -0300

    Implementation with error -> List Object != List Integer

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Object> myObjs = new ArrayList<Object>();
+		List<Integer> myNumbers = new ArrayList<Integer>();
+		myObjs = myNumbers;
+		
+	}
+}

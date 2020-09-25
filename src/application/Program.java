090b94d4c53155c3430c464257b8f18ebd433cf4
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 11:59:16 -0300

    Implementation Any Type

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,17 @@
 package application;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
-
 	public static void main(String[] args) {
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+	}
 
-		List<?> myObjs = new ArrayList<Object>();
-		List<Integer> myNumbers = new ArrayList<Integer>();
-		myObjs = myNumbers;
-		
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
